9f3e0f8fd679601e48d5456f3c8a706227113a83
Grzegorz Smoktunowicz <biarth@wp.pl>
2023-03-08 18:40:04 +0100

    first commit

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const databaseConnect = (uri) => {
+  mongoose
+    .connect(uri, {})
+    .then(() => {
+      console.log("Mongodb Database Connected");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+module.exports = databaseConnect;
