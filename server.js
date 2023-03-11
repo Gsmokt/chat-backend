2512419cb0a26c476f91f6e1661ebee1087325ca
Grzegorz Smoktunowicz <biarth@wp.pl>
2023-03-11 11:38:38 +0100

    scoket

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const io = socketio(httpServer, {
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader(
     "Access-Control-Allow-Methods",
